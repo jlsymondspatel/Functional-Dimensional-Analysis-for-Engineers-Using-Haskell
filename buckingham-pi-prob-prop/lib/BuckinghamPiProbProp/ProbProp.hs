c68038ddecf34ce9fa12e07abf3405fccf7ad64e
jlsymondspatel <jlsymondspatel>
2023-06-15 15:58:42 +0100

    updated comments in code

--- buckingham-pi-prob-prop/lib/BuckinghamPiProbProp/ProbProp.hs
+++ buckingham-pi-prob-prop/lib/BuckinghamPiProbProp/ProbProp.hs
@@ -1,11 +1,15 @@
 module BuckinghamPiProbProp.ProbProp where
 
+-- import Statistics library in order to use PDF functions for several
+-- probability distributions
 import qualified Statistics.Distribution as SD
 import qualified Statistics.Distribution.Normal as SDN
 import qualified Statistics.Distribution.Uniform as SDU
+-- import Applicative library in order to use liftA2 and the Applicative
+-- data type
 import Control.Applicative
 
-
+-- define data type to represent the context of a "Y space"
 data YS a = YS a
   deriving (Show,Ord,Eq)
 
@@ -16,6 +20,7 @@ instance Applicative YS where
   pure = YS
   YS func <*> YS x = YS (func x)
 
+-- define data type to represent the context of a "CDF value space"
 data CDF a = CDF a
   deriving (Show,Ord,Eq)
 
@@ -26,6 +31,7 @@ instance Applicative CDF where
   pure = CDF
   CDF func <*> CDF x = CDF (func x)
 
+-- define data type to represent the context of a "PDF value space"
 data PDF a = PDF a
   deriving (Show,Ord,Eq)
 
@@ -36,13 +42,17 @@ instance Applicative PDF where
   pure = PDF
   PDF func <*> PDF x = PDF (func x)
 
+-- utility functions to convert from and to a PDF/CDF value space
 fsCDFtoPDF (CDF a) = PDF a
 fsPDFtoCDF (PDF a) = CDF a
 
-
+-- define type synonyms that define the funcitonal type for a transformation of
+-- random variables, g
 type Transform = [Double] -> YS Double
+-- define type synonyms that define the funcitonal type for a known original PDF
 type OriginalPDF = [Double] -> PDF Double
 
+-- define known PDF functions of known random variables
 normpdf :: Double -> Double -> Double -> Double
 normpdf mu sigma x = SD.density (SDN.normalDistr mu sigma) x
 
@@ -52,26 +62,33 @@ normstdpdf =  SD.density SDN.standard
 unifpdf :: Double -> Double -> Double -> Double
 unifpdf a b x = SD.density (SDU.uniformDistr a b) x
 
-dx1 = 0.1 :: Double -- l
-dx2 = 0.1 :: Double -- g
-dx3 = 0.1 :: Double -- tau
+-- define the spacing of the domain of the known (joint) PDF
+dx1 = 0.1 :: Double
+dx2 = 0.1 :: Double
+dx3 = 0.1 :: Double
 dxs = [dx1,dx2,dx3]
 
+-- define the ranges of each argument to the domain of the known (joint) PDF
 x1s = [12,(12+dx1)..18] -- l
 x2s = [7,(7+dx2)..13] -- g
 x3s = [2,(2+dx3)..8] -- tau
 xss = [x1s,x2s,x3s]
 
+-- define the function that transform the known random variables
 g :: Transform
 g [x1,x2,x3] = pure ((x1**(-0.5)) * (x2**0.5) * x3)
 g _ = pure 0
 
+-- define the whole joint PDF of all known random variables which may or may
+-- not be independent
 fPDF :: OriginalPDF
 fPDF [x1,x2,x3] = pure ((normpdf 15 1 x1) * (normpdf 9.8 1 x2) * (normpdf 5.4 1 x3))
 fPDF _ = pure 0
 
 dy = YS 0.1
 
+-- define the pushforward distribution function that obtains a PDF value
+-- of the transformed random variable Y
 pushPDF :: OriginalPDF -> Transform -> YS Double -> YS (PDF Double)
 pushPDF f g y = fmap fsCDFtoPDF $ (fmap . fmap) (/ ((\(YS u) -> u) dy)) dFy
   where pushCDF yval = pure $ fsPDFtoCDF $ fmap (product dxs *) . fmap sum . sequenceA $ fCartProdFilt :: YS (CDF Double)
--- buckingham-pi/lib/BuckinghamPi/PiTerms.hs
+++ buckingham-pi/lib/BuckinghamPi/PiTerms.hs
@@ -1,84 +1,123 @@
 module BuckinghamPi.PiTerms where
 
+-- import the following two libraries for the matrix data type, and matrix
+-- functions
 import qualified Data.Matrix as DM (Matrix, transpose, fromLists, toLists, detLU, zero, rref,
                                     (<|>), ncols, colVector, getCol, submatrix, (<->), scaleMatrix,
                                     identity, luDecomp', multStd, matrix, inverse)
 import qualified Numeric.Matrix as NM (Matrix, fromList, toList, rank, zero, inv, isEmpty, isZero)
+-- import Ratio library to handle the Rational data type
 import Data.Ratio
+-- import Maybe library to handle the Maybe data type
 import Data.Maybe
+-- import List library for extra capability in list permutations, as well as
+-- importing two more list functions for list searching and testing uniqueness
 import Data.List
 import Data.List.Split (endBy)
 import Data.List.Unique (isUnique)
 
 
+-- create the Dim data type to represent dimensions of physical quantities
+-- where there are a selection of 26 dimensions (A to Z)
 data Dim = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z
   deriving (Eq, Show, Ord, Read)
 
-
+-- define type synonym Pow to represent the power of a dimension 
 type Pow = Rational
+-- define type synonym Var to represent the name of a physical variable 
 type Var = String
+-- define type synonym DimQu to represent a dimensional quantity
 type DimQu = [(Dim,Pow)]
+-- define a type synonym to represent a dimensional matrix
 type DimMat = DM.Matrix Pow
+-- define a type synonym to represent a kernel matrix containing
+-- the kernel vectors of a dimensional matrix
 type Kernel = DM.Matrix Pow
+
+-- the above two type synonyms imply that the DM matrix type is the default type
 
 
 -- ###################################################
 -- # Helpful "gluing" functions that are fundamental #
 -- ###################################################
-
+-- a function to sort a dimensional quantity, DimQu, so that dimensions
+-- in [(dim,pow)] are not repeated, but added/subtracted together
 sortDimQu' :: DimQu -> DimQu
 sortDimQu' ((dim,pow):[]) = [(dim,pow)]
 sortDimQu' ((dim1,pow1):(dim2,pow2):otherdims)
   | dim1 == dim2 = sortDimQu' ((dim1,pow1 + pow2):otherdims)
   | dim1 /= dim2 = (dim1,pow1) : sortDimQu' ((dim2,pow2):otherdims)
-
+-- a function to sort a dimensional quantity, DimQu, including a preprocessing
+-- of the input so all dimensions are in alphabetical order
 sortDimQu :: DimQu -> DimQu
 sortDimQu = sortDimQu' . sort
 
+-- a function to sort a list of dimensional quantities, [DimQu]
 sortDimQus :: [DimQu] -> [DimQu]
 sortDimQus = map sortDimQu
 
+-- a function that multiplies two dimensional quantities (meaning the powers of
+-- their common dimensions are added together)
 (|*|) :: DimQu -> DimQu -> DimQu
 (|*|) dimqu_1 dimqu_2 = sortDimQu $ dimqu_1 ++ dimqu_2
 
+-- a function that multiplies two dimensional quantities (meaning the powers of
+-- their common dimensions are added together)
 (|/|) :: DimQu -> DimQu -> DimQu
 (|/|) dimqu_1 dimqu_2 = sortDimQu $ dimqu_1 ++ (map (\(dim,pow) -> (dim,(-pow))) dimqu_2)
 
+-- function to create a column of a dimensional matrix as a list of powers of
+-- all separate dimensions in a dimensional quantity
 makeDimMatCol :: DimQu -> [Pow]
 makeDimMatCol [] = []
 makeDimMatCol ((dim,pow):otherdims) = pow : makeDimMatCol otherdims
 
-makeDimMat :: [DimQu] -> DM.Matrix Pow
+-- function to make a dimensional matrix from a list of dimensional quantities
+makeDimMat :: [DimQu] -> DimMat
 makeDimMat = DM.transpose . DM.fromLists . map makeDimMatCol . sortDimQus
 
+-- function to convert a matrix from the DM library to a matrix in the NM
+-- library's data type
 toNM :: DM.Matrix Pow -> NM.Matrix Pow
 toNM = NM.fromList . DM.toLists
 
+-- function to convert a matrix from the NM library to a matrix in the DM
+-- library's data type
 toDM :: NM.Matrix Pow -> DM.Matrix Pow
 toDM = DM.fromLists . NM.toList
 
+-- function to convert a RealFrac number to an integer by rounding it
 toInt n = (round n) :: Int 
 
+-- function to get the rank of a (dimensional) matrix
 getRank :: DM.Matrix Pow -> Int
 getRank =  toInt . NM.rank . toNM
 
+-- function to get the determinant of a (dimensional) matrix
 getDet :: DM.Matrix Pow -> Int
 getDet = toInt . DM.detLU
 
+-- function to get the inverse of a matrix
 getInverse :: DM.Matrix Pow -> Maybe (DM.Matrix Pow)
 getInverse = (either (\_ -> Nothing) Just) . DM.inverse
 
+-- function to get the reduced row echelon form of a matrix
 getRREF :: DM.Matrix Pow -> Maybe (DM.Matrix Pow)
 getRREF = (either (\_ -> Nothing) Just) . DM.rref
 
+-- function to find the unique combinations of elements from an original list
+-- where the unique combinations are only of a certain length, n
+-- example: combinations 2 "abc" == ["ab","ac","bc"]
 combinations :: Int -> [a] -> [[a]]
 combinations n = filter ((== n) . length) . subsequences
 
-getNPiTerms :: DM.Matrix Pow -> Int
+-- function to get the number of pi terms expected from a dimensional matrix
+getNPiTerms :: DimMat -> Int
 getNPiTerms dimMat = n - k
   where n = DM.ncols dimMat
         k = getRank dimMat
 
+-- function to check if a matrix is composed of all zero elements
 isMatZero :: DM.Matrix Pow -> Bool
 isMatZero = NM.isZero . toNM
 
@@ -86,6 +125,11 @@ isMatZero = NM.isZero . toNM
 -- #####################
 -- #  LU Decomposition #
 -- #####################
+
+-- LU decomposition is needed so that the number of pi terms of a dimensional
+-- matrix can be obtained even when that dimensional matrix may have column
+-- configurations where the whole matrix is rank deficient. This assumes that
+-- getRank can only work on square matrix segments of rectangular matrices
 
 -- function to get LU decomnposition with full pivoting
 -- (where the output is (U,L,P,Q,d,e):
@@ -97,6 +141,7 @@ isMatZero = NM.isZero . toNM
 getLUDecomp :: DM.Matrix Pow -> (DM.Matrix Pow, DM.Matrix Pow, DM.Matrix Pow, DM.Matrix Pow, Pow, Pow)
 getLUDecomp = fromMaybe (DM.zero 1 1,DM.zero 1 1,DM.zero 1 1,DM.zero 1 1,0,0) . DM.luDecomp'
 
+-- function to only obtain the 
 getLUDecompU :: DM.Matrix Pow -> DM.Matrix Pow
 getLUDecompU = (\(u,_,_,_,_,_) -> u) . getLUDecomp
 
@@ -105,15 +150,21 @@ getLUDecompU = (\(u,_,_,_,_,_) -> u) . getLUDecomp
 -- # Method of Repeating Variables (Helper Functions) #
 -- ####################################################
 
+-- function to get a list of unique combinations of non repeating variables
+-- from a list
 getNonRepVarsList :: Int -> [a] -> [[(Int,a)]]
 getNonRepVarsList n ls = combinations n (zip [1..nLs] ls)
   where nLs = length ls
 
+-- function to get a list of all unique combinations of non-repeating variables
 getRepVarsCombsPM :: Eq a => Int -> [a] -> [[a]]
 getRepVarsCombsPM n dimQus = map (snd . unzip) [(dimQusNumd \\ nonRepVars) ++ nonRepVars | nonRepVars <- nonRepVarsListNumd]
   where nonRepVarsListNumd = getNonRepVarsList n dimQus
         dimQusNumd = zip [1..(length dimQus)] dimQus
 
+-- function to split up a dimensional matrix into a matrix of repeating
+-- variables (called X, or P), and a matrix of non-repeating variables
+-- (called Y, or Q)
 getXY :: DM.Matrix Pow -> (DM.Matrix Pow,DM.Matrix Pow)
 getXY dimMat = (x,y)
   where rank = getRank dimMat
@@ -126,19 +177,27 @@ getXY dimMat = (x,y)
 -- #  Method of Repeating Variables (Main Functions)  #
 -- ####################################################
 
+-- function to get all unique combinations of repeating variables and
+-- non-repeating variables
 getRepVarsCombs :: ([Var],[DimQu]) -> [([Var],[DimQu])]
 getRepVarsCombs (vars,dimQus) = zip varsCombs dimQusCombs
   where nNonRepVars = getNPiTerms . getLUDecompU . makeDimMat $ dimQus
         dimQusCombs = getRepVarsCombsPM nNonRepVars dimQus :: [[DimQu]]
         varsCombs = getRepVarsCombsPM nNonRepVars vars :: [[Var]]
 
+-- function to get the dimensional matrices from the list of all
+-- unique combinations of repeating/non-repeating variables
 getDimMatFromDimQus :: [([Var],[DimQu])] -> [([Var],DimMat)]
 getDimMatFromDimQus varsDimQusList = (map . fmap) makeDimMat varsDimQusList
 
+-- function to filter out dimensional matrices that are singular due to their
+-- column configurations
 getNonSingDimMats :: [([Var],DimMat)] -> [([Var],DimMat)]
 getNonSingDimMats varsDimMatList = filter filterFunc varsDimMatList
   where filterFunc = \(_,dimMat) -> if (getDet dimMat /= 0) then True else False
 
+-- function to filter out all dimensional matrices that would yield duplicate
+-- pi terms via filtering out dimensional matrices that have the same RREF
 getNonDupVarsDimQus :: [([Var],DimMat)] -> [([Var],DimMat)]
 getNonDupVarsDimQus varsDimMatList = nonDupVarsRREFDimMatList
   where rrefDimMats = map getRREF . snd . unzip $ varsDimMatList
@@ -147,6 +206,10 @@ getNonDupVarsDimQus varsDimMatList = nonDupVarsRREFDimMatList
         nonDupVarsRREFDimMatList =
           [fromMaybe ([""],DM.zero 1 1) . find ((== nonDupRREFDimMat) . ((fromMaybe (DM.zero 1 1)) . getRREF) . snd) $ varsDimMatList | nonDupRREFDimMat <- nonDupRREFDimMats]
 
+-- function to obtain the kernel matrix from all dimensional matrices using the
+-- equation v = -inv(P)*Q*e or v = -inv(X)*Y*e where v and e constitute the
+-- unknown and known (canonical basis vector matrix, e) parts of the kernel
+-- matrix
 getKernels :: [([Var],DimMat)] -> [([Var],Kernel)]
 getKernels varsDimMatList = (map . fmap) getKernel varsDimMatList
   where getKernel dimMat = (DM.scaleMatrix (-1) (DM.multStd xInv (DM.multStd y canVecBasMat))) DM.<-> canVecBasMat
@@ -155,6 +218,7 @@ getKernels varsDimMatList = (map . fmap) getKernel varsDimMatList
                 xInv = fromMaybe (DM.zero 1 1) $ getInverse x
                 canVecBasMat = DM.identity $ getNPiTerms dimMatRREF
 
+-- a function wrap all main functions together in sequence
 getSetsOfPiTerms :: ([Var],[DimQu]) -> [([Var],Kernel)]
 getSetsOfPiTerms = getKernels .
                    getNonDupVarsDimQus .
